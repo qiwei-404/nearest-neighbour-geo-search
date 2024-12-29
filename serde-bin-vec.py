bc56837918148296aed098621e8892cd8b2ab573
Andrew Matte <andrew_matte_@hotmail.com>
2024-12-29 02:20:21 -0800

    housekeeping

--- serde-bin-vec.py
+++ serde-bin-vec.py
@@ -7,21 +7,20 @@ import struct
 import binascii
 
 
-num_bytes = 32
-vec_length = 1024
+# num_bytes = 32
+# vec_length = 1024
 
 
 def save(uuid, vector: list, geo = [0,0], filename='data.bin'):
+    to_write = uuid.encode()
+    to_write += b''.join([struct.pack('f', f) for f in vector])
+    to_write += b''.join([struct.pack('f', f) for f in geo])
     f = open(filename, 'ab')
-    f.write(uuid.encode())
-    f = open(filename, 'ab')
-    data = [struct.pack('f', f) for f in vector]
-    [f.write(d) for d in data]
-    [f.write(d) for d in [struct.pack('f', f) for f in geo]]
+    f.write(to_write)
     f.close()
 
 
-def read_all(filename='data.bin'):
+def read_all(filename='data.bin', num_bytes, vec_length):
     f = open(filename, 'rb')
     output = []
     Is = 'B'*num_bytes
@@ -39,6 +38,6 @@ def read_all(filename='data.bin'):
     return output
 
 
-def save_all(list_of_lists):
+def save_all(list_of_lists, filename):
     for item in list_of_lists:
-        save(item[0], item[1])
+        save(item[0], item[1], [0,0], filename)
